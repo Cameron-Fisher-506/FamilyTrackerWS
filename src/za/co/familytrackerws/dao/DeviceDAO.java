7bbc21e727b5ff85acb0261987fa0507890ab84e
Cameron <cameron@aheadofthecurve.co.za>
2020-11-07 16:32:09 +0200

    Added the ability to get, update, link, unlink devices.
    - DeviceWS: Added getDeviceCoordinateHealth, updateCoordinateHealth,
    linkDevice, unlinkDevice services.
    - DeviceDAO: Added getDeviceCoordinateHealth,
    updateDeviceCoordinateHealth, linkDevice, unlinkDevice methods.

--- src/za/co/familytrackerws/dao/DeviceDAO.java
+++ src/za/co/familytrackerws/dao/DeviceDAO.java
@@ -1,10 +1,16 @@
 package za.co.familytrackerws.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.json.JSONObject;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+
+import za.co.familytrackerws.utils.DTUtils;
 import za.co.familytrackerws.utils.MongoUtils;
 
 public class DeviceDAO 
@@ -83,6 +89,221 @@ public class DeviceDAO
 				toReturn.put("message", message);
 			}
 			
+		}
+		
+		return toReturn;
+	}
+	
+	public static JSONObject getDeviceCoordinateHealth(String imei)
+	{
+		JSONObject toReturn = null;
+		
+		MongoCollection<Document> deviceCollection = MongoUtils.mongoDatabase.getCollection(MONGO_DB_COLLECTION_NAME);
+		
+		if(imei != null)
+		{
+			Document device = deviceCollection.find(Filters.eq("imei", imei)).first();
+			if(device != null)
+			{
+				toReturn = new JSONObject();
+				if(device.containsKey("coordinate"))
+				{
+					Document coordinate = (Document) device.get("coordinate");
+					if(coordinate != null)
+					{
+						toReturn.put("coordinate", coordinate.toJson());
+					}
+				}
+				
+				if(device.containsKey("health"))
+				{
+					Document health = (Document)device.get("health");
+					if(health != null)
+					{
+						toReturn.put("health", health.toJson());
+					}
+				}
+				
+			}
+		}
+		
+		return toReturn;
+	}
+	
+	public static boolean updateDeviceCoordinateHealth(String body)
+	{
+		boolean toReturn = false;
+		
+		MongoCollection<Document> deviceCollection = MongoUtils.mongoDatabase.getCollection(MONGO_DB_COLLECTION_NAME);
+		if(deviceCollection != null)
+		{
+			JSONObject jsonObject = new JSONObject(body);
+			if(jsonObject != null && jsonObject.has("imei"))
+			{
+				String imei = jsonObject.getString("imei");
+				if(imei != null)
+				{
+					if(jsonObject.has("coordinate"))
+					{
+						JSONObject coordinate = jsonObject.getJSONObject("coordinate");
+						if(coordinate != null && coordinate.has("latitude") && coordinate.has("longitude"))
+						{	
+							Document document = deviceCollection.findOneAndUpdate(Filters.eq("imei", imei), Updates.combine(
+									Updates.set("coordinate.latitude", coordinate.getString("latitude")),
+									Updates.set("coordinate.longitude", coordinate.getString("longitude")),
+									Updates.set("coordinate.createdTime", DTUtils.getCurrentDateTime())
+									));
+							
+							if(document != null)
+							{
+								toReturn = true;
+							}else
+							{
+								toReturn = false;
+							}
+						}
+					}
+					
+					if(jsonObject.has("health"))
+					{
+						JSONObject health = jsonObject.getJSONObject("health");
+						if(health != null && health.has("batteryLife") && health.has("signalStrength"))
+						{
+							Document document = deviceCollection.findOneAndUpdate(Filters.eq("imei", imei), Updates.combine(
+									Updates.set("health.batteryLife", health.getString("batteryLife")),
+									Updates.set("health.signalStrength", health.getString("signalStrength")),
+									Updates.set("health.createdTime", DTUtils.getCurrentDateTime())
+									));
+							
+							if(document != null)
+							{
+								toReturn = true;
+							}else
+							{
+								toReturn = false;
+							}
+						}
+					}
+				}
+			}
+		}
+		return toReturn;
+	}
+	
+	public static boolean linkDevice(String body)
+	{
+		boolean toReturn = false;
+		
+		MongoCollection<Document> deviceCollection = MongoUtils.mongoDatabase.getCollection(MONGO_DB_COLLECTION_NAME);
+		if(deviceCollection != null)
+		{
+			if(body != null)
+			{
+				JSONObject jsonObject = new JSONObject(body);
+				if(jsonObject != null && jsonObject.has("imei") && jsonObject.has("imeiToLink"))
+				{
+					String imei = jsonObject.getString("imei");
+					if(imei != null)
+					{
+						Document device = deviceCollection.find(Filters.eq("imei", imei)).first();
+						if(device != null && device.containsKey("monitors"))
+						{
+							
+							List<Document> monitors = (List<Document>) device.get("monitors");
+							
+							if(monitors != null && monitors.size() > 0)
+							{
+								boolean isExists = false;
+								for(int i = 0; i < monitors.size(); i++)
+								{
+									Document monitor = monitors.get(i);
+									if(monitor != null && monitor.containsKey("imei"))
+									{
+										if(monitor.getString("imei").equals(imei))
+										{
+											isExists = true;
+										}
+									}
+								}
+								
+								if(!isExists)
+								{						
+									Document toPush = new Document();
+									toPush.put("imei", jsonObject.getString("imeiToLink"));
+									
+									Document document = deviceCollection.findOneAndUpdate(Filters.eq("imei", imei), Updates.push("monitors", toPush));
+									if(document != null)
+									{
+										toReturn = true;
+									}
+								}
+							}
+						}
+						
+					}
+					
+				}
+			}
+		}
+		
+		return toReturn;
+	}
+	
+	public static boolean unlinkDevice(String body)
+	{
+		boolean toReturn = false;
+		
+		
+		MongoCollection<Document> deviceCollection = MongoUtils.mongoDatabase.getCollection(MONGO_DB_COLLECTION_NAME);
+		if(deviceCollection != null)
+		{
+			if(body != null)
+			{
+				JSONObject jsonObject = new JSONObject(body);
+				if(jsonObject != null && jsonObject.has("imei") && jsonObject.has("imeiToUnlink"))
+				{
+					String imei = jsonObject.getString("imei");
+					if(imei != null)
+					{
+						Document device = deviceCollection.find(Filters.eq("imei", imei)).first();
+						if(device != null && device.containsKey("monitors"))
+						{
+							
+							List<Document> monitors = (List<Document>) device.get("monitors");
+							
+							if(monitors != null && monitors.size() > 0)
+							{
+								boolean isExists = false;
+								for(int i = 0; i < monitors.size(); i++)
+								{
+									Document monitor = monitors.get(i);
+									if(monitor != null && monitor.containsKey("imei"))
+									{
+										if(monitor.getString("imei").equals(imei))
+										{
+											isExists = true;
+										}
+									}
+								}
+								
+								if(!isExists)
+								{						
+									Document toRemove = new Document();
+									toRemove.put("imei", jsonObject.getString("imeiToUnlink"));
+									
+									Document document = deviceCollection.findOneAndUpdate(Filters.eq("imei", imei), Updates.pull("monitors", toRemove));
+									if(document != null)
+									{
+										toReturn = true;
+									}
+								}
+							}
+						}
+						
+					}
+					
+				}
+			}
 		}
 		
 		return toReturn;
--- src/za/co/familytrackerws/ws/DeviceWS.java
+++ src/za/co/familytrackerws/ws/DeviceWS.java
@@ -1,8 +1,10 @@
 package za.co.familytrackerws.ws;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,6 +28,78 @@ public class DeviceWS
 		if(jsonObject != null)
 		{
 			toReturn = DeviceDAO.create(jsonObject);
+		}
+		
+		return Response.status(200).entity(toReturn.toString()).build();
+	}
+	
+	@Path("getDeviceCoordinateHealth/{imei}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getDeviceCoordinateHealth(@PathParam("imei") String imei)
+	{
+		JSONObject toReturn = null;
+		
+		toReturn = DeviceDAO.getDeviceCoordinateHealth(imei);
+		if(toReturn == null)
+		{
+			return Response.status(404).entity(-1).build();
+		}
+		
+		return Response.status(200).entity(toReturn.toString()).build();
+	}
+	
+	@Path("updateDeviceCoordinateHealth")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response updateCoordinateHealth(String body)
+	{
+		JSONObject toReturn = new JSONObject();
+		
+		if(DeviceDAO.updateDeviceCoordinateHealth(body))
+		{
+			toReturn.put("code", 0);
+		}else {
+			toReturn.put("code", -1);
+		}
+		
+		return Response.status(200).entity(toReturn.toString()).build();
+	}
+	
+	@Path("linkDevice")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response linkDevice(String body)
+	{
+		JSONObject toReturn = new JSONObject();
+		
+		boolean isLinked = DeviceDAO.linkDevice(body);
+		if(isLinked)
+		{
+			toReturn.put("code", 0);
+		}else {
+			toReturn.put("code", -1);
+		}
+		
+		return Response.status(200).entity(toReturn.toString()).build();
+	}
+	
+	@Path("unlinkDevice")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response unlinkDevice(String body)
+	{
+		JSONObject toReturn = new JSONObject();
+		
+		boolean isLinked = DeviceDAO.unlinkDevice(body);
+		if(isLinked)
+		{
+			toReturn.put("code", 0);
+		}else {
+			toReturn.put("code", -1);
 		}
 		
 		return Response.status(200).entity(toReturn.toString()).build();
